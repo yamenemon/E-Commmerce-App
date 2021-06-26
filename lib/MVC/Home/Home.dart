dc80833629d24084c6b51232fcb814fd98720efe
Yamen Emon <khaliful.emon@sslwireless.com>
2021-06-26 13:15:17 +0600

    stagger gridview added

--- lib/MVC/Home/Home.dart
+++ lib/MVC/Home/Home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -7,15 +8,21 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: [
-            Container(
-              child: Text("data"),
-            ),
-            Container(
-              child: Text("data"),
-            )
-          ],
+        body: StaggeredGridView.countBuilder(
+          crossAxisCount: 4,
+          itemCount: 12,
+          itemBuilder: (BuildContext context, int index) => Container(
+              color: Colors.green,
+              child: Center(
+                child: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Text('$index'),
+                ),
+              )),
+          staggeredTileBuilder: (int index) =>
+              StaggeredTile.count(2, index.isEven ? 2 : 1),
+          mainAxisSpacing: 4.0,
+          crossAxisSpacing: 4.0,
         ),
       ),
     );
