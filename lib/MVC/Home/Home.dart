9675785d5d5012669d8de46d71daedbd4649f814
Yamen Emon <khaliful.emon@sslwireless.com>
2021-06-26 13:18:20 +0600

    removed debug mode

--- lib/MVC/Home/Home.dart
+++ lib/MVC/Home/Home.dart
@@ -6,24 +6,22 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: StaggeredGridView.countBuilder(
-          crossAxisCount: 4,
-          itemCount: 12,
-          itemBuilder: (BuildContext context, int index) => Container(
-              color: Colors.green,
-              child: Center(
-                child: CircleAvatar(
-                  backgroundColor: Colors.white,
-                  child: Text('$index'),
-                ),
-              )),
-          staggeredTileBuilder: (int index) =>
-              StaggeredTile.count(2, index.isEven ? 2 : 1),
-          mainAxisSpacing: 4.0,
-          crossAxisSpacing: 4.0,
-        ),
+    return Scaffold(
+      body: StaggeredGridView.countBuilder(
+        crossAxisCount: 4,
+        itemCount: 12,
+        itemBuilder: (BuildContext context, int index) => Container(
+            color: Colors.green,
+            child: Center(
+              child: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Text('$index'),
+              ),
+            )),
+        staggeredTileBuilder: (int index) =>
+            StaggeredTile.count(2, index.isEven ? 2 : 1),
+        mainAxisSpacing: 4.0,
+        crossAxisSpacing: 4.0,
       ),
     );
   }
