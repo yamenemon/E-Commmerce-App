94844ad5f0f6d23f6f969fd01ba479ab52c188ca
Yamen Emon <khaliful.emon@sslwireless.com>
2021-06-26 12:44:46 +0600

    splash screen added

---
+++ lib/MVC/Home/Home.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Container(
+              child: Text("data"),
+            ),
+            Container(
+              child: Text("data"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
